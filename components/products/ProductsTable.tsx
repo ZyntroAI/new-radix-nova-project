60483fe1c60c6dd6a860148f51ef01731966186c
Zyntro <zyntro.ai.studio@gmail.com>
2026-08-30 18:48:42 +0700

    Create ProductsTable.tsx
    Signed-off-by: Zyntro <zyntro.ai.studio@gmail.com>

---
+++ components/products/ProductsTable.tsx
@@ -0,0 +1,6 @@
+<Card>
+  <Input placeholder="ค้นหาสินค้า..." /> {/* input */}
+  <Table> {/* table */} </Table>
+  <Pagination /> {/* pagination */}
+  <DropdownMenu> {/* dropdown-menu ต่อแถว */} </DropdownMenu>
+</Card>
