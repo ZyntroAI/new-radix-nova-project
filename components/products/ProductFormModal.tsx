31a9310c06f295c3c204ec99724dbc12c0e26895
Zyntro <zyntro.ai.studio@gmail.com>
2026-08-30 18:48:58 +0700

    Create ProductFormModal.tsx
    Signed-off-by: Zyntro <zyntro.ai.studio@gmail.com>

---
+++ components/products/ProductFormModal.tsx
@@ -0,0 +1,7 @@
+<Dialog> {/* dialog */}
+  <Form> {/* form + react-hook-form + zod */}
+    <Select /> {/* select category */}
+    <Switch /> {/* switch is_active */}
+  </Form>
+</Dialog>
+<AlertDialog> {/* alert-dialog สำหรับ delete */} </AlertDialog>
