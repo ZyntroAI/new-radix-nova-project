6e772ab1c97b421f71cd9e6831eb9edeb065418e
Zyntro <zyntro.ai.studio@gmail.com>
2026-08-30 18:52:27 +0700

    Create shadcn.sh
    Signed-off-by: Zyntro <zyntro.ai.studio@gmail.com>

---
+++ backend/src/utils/sh/shadcn.sh
@@ -0,0 +1,2 @@
+npm i @tanstack/react-query axios react-hook-form @hookform/resolvers zod
+npx shadcn@latest add table pagination input dialog form label select textarea switch badge dropdown-menu card skeleton button sonner
